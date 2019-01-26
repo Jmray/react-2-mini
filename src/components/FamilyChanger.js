e4cfd31893d8615e1b05a0ac649d6bf30d088caa
Justus Ray <justusmray@gmail.com>
2019-01-26 11:27:40 -0700

    this is the finished app

--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function FamilyChanger() {
+export default function FamilyChanger(props) {
+  
   return (
-    <select className="dropDownContainer">
+    <select className="dropDownContainer" onChange={event => props.update(event.target.value)}>
       <option value="monospace"> Monospace </option>
       <option value="arial"> Arial </option>
       <option value="courier"> Courier </option>
--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 
 export default class TextContainer extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       text: ''
     }
+    this.updateText = this.updateText.bind(this);
   }
 
   updateText(e) {
     this.setState({
       text: e.target.value
-    })
+    });
   }
 
   render() {
     return (
       <div className="textContainer">
         <textarea 
-          // style={  }
+          style={
+            {fontFamily: this.props.fontFamily,
+            fontSize: this.props.fontSize,
+            color: this.props.fontColor,}
+
+            }
           onChange={this.updateText}
           value={this.state.text} 
           placeholder='Start typing your thoughts here!'
